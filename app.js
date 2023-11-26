9b4a60785fb4dc041086559fe12b95b8ed088ae7
Brinda <brindashree29@gmail.com>
2023-11-26 19:39:51 +0530

    feat: user stories

--- app.js
+++ app.js
@@ -2,6 +2,66 @@ const parentAcc = document.getElementById("parent-accordion");
 const stepAcc = document.querySelectorAll(".step-accordion");
 const notificationIconEle = document.getElementById("notification-icon");
 const navProfileTagEle = document.getElementById("menu-trigger");
+const crossIconBtn = document.getElementById("cancel-cross-icon");
+const trialCalloutEle = document.getElementById("trial-callout");
+const panelArrowEle = document.getElementById("panel-toggle");
+const stepsCompletedEle = document.getElementById("steps-completed");
+const progressBarEle = document.getElementById("progress-bar");
+
+let rotated = false;
+let stepsCompleted = 0;
+function completeStep(number) {
+  const svgContainer = document.getElementById(`svgContainer${number}`);
+  if (svgContainer?.classList?.contains("completed")) {
+    svgContainer.innerHTML = `<svg
+    id="svg${number}"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    >
+    <circle
+      cx="12"
+      cy="12"
+      r="10"
+      stroke="#8A8A8A"
+      stroke-width="2.08333"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    </svg>`;
+    svgContainer.classList.remove("completed");
+    stepsCompleted--;
+    stepsCompletedEle.innerText = `${stepsCompleted} / 5 completed`;
+  } else {
+    stepsCompleted++;
+    stepsCompletedEle.innerText = `${stepsCompleted} / 5 completed`;
+    svgContainer.classList.add("completed");
+    svgContainer.innerHTML = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <circle cx="12" cy="12" r="10" fill="#303030"></circle>
+  <path
+    d="M17.2738 8.52629C17.6643 8.91682 17.6643 9.54998 17.2738 9.94051L11.4405 15.7738C11.05 16.1644 10.4168 16.1644 10.0263 15.7738L7.3596 13.1072C6.96908 12.7166 6.96908 12.0835 7.3596 11.693C7.75013 11.3024 8.38329 11.3024 8.77382 11.693L10.7334 13.6525L15.8596 8.52629C16.2501 8.13577 16.8833 8.13577 17.2738 8.52629Z"
+    fill="#fff"
+  ></path>
+</svg>`;
+  }
+  progressBarEle.style.display = "block";
+  progressBarEle.style.width = `${stepsCompleted * 20}%`;
+}
+
+function rotateImage() {
+  if (rotated) {
+    panelArrowEle.style.transform = "rotate(0deg)";
+  } else {
+    panelArrowEle.style.transform = "rotate(180deg)";
+  }
+  rotated = !rotated;
+}
+
+crossIconBtn.addEventListener("click", () => {
+  trialCalloutEle.style.visibility = "hidden";
+});
 
 navProfileTagEle.addEventListener("click", () =>
   toggleDisplay(".menu-wrapper")
@@ -10,7 +70,7 @@ notificationIconEle.addEventListener("click", () =>
   toggleDisplay(".alert-wrapper")
 );
 
-parentAcc.addEventListener("click", () => {
+panelArrowEle.addEventListener("click", () => {
   const panel = document.getElementById("panel");
   panel.style.display = panel.style.display === "block" ? "none" : "block";
 
@@ -23,8 +83,8 @@ parentAcc.addEventListener("click", () => {
 });
 
 stepAcc.forEach((accordion) => {
-  accordion.addEventListener("click", (event) => {
-    event.stopPropagation();
+  accordion.addEventListener("click", (e) => {
+    e.stopPropagation();
 
     document.querySelectorAll(".step-accordion").forEach((otherAccordion) => {
       if (otherAccordion !== accordion) {
@@ -34,10 +94,11 @@ stepAcc.forEach((accordion) => {
           otherPanel.style.display = "none";
       }
     });
-
-    accordion.classList.toggle("active");
-    const panel = accordion.querySelector(".step-description");
-    panel.style.display = panel.style.display === "flex" ? "none" : "flex";
+    if (!accordion?.classList?.contains("active")) {
+      accordion.classList.toggle("active");
+      const panel = accordion.querySelector(".step-description");
+      panel.style.display = panel.style.display === "flex" ? "none" : "flex";
+    }
   });
 });
 
@@ -92,4 +153,7 @@ function restoreSVG(stepNumber) {
   />
 </svg>
   `;
+}
+function redirect(url) {
+  window.open(url, "_blank");
 }
